70b261ec10e089a879d70fdff5018c3e7b5d43f9
HENADII SYCHEVSKYI <SGI@HENADIIs-MacBook-Pro-4.local>
2020-09-28 16:28:59 +0300

    Added script for get cluster envs

---
+++ scripts/getClusterEnvs.js
@@ -0,0 +1,57 @@
+function getList(envName, items) {
+    var result = [],
+        index = 0,
+        itemData,
+        targetEnvNamePrefix;
+
+    function dataExtractor(envName) {
+        var envNameSeparator = "-",
+            index,
+            tmp;
+
+        tmp = envName.split(envNameSeparator);
+        index = tmp.pop();
+
+        return {
+            envName: envName, 
+            envNamePrefix: tmp.join(envNameSeparator),
+            index: parseInt(index, 10)
+        };
+
+    }
+
+    function filter(data) {
+        return data.envNamePrefix === targetEnvNamePrefix && 
+                typeof data.index === "number" && !isNaN(data.index);
+    }
+
+        
+    targetEnvNamePrefix = dataExtractor(envName).envNamePrefix; 
+
+    for(var i=0, item; item = items[i]; i++) {
+        itemData = dataExtractor(item);
+
+        if (itemData && filter(itemData)) {
+            result.push(itemData.envName);
+            index = Math.max(index, itemData.index);
+        }        
+    }
+
+    return {
+        items       : result,
+        nextIndex   : index + 1
+    };
+}
+
+
+var resp = jelastic.environment.control.GetEnvs(appid, session);
+
+if (resp.result != 0) {
+    result resp;
+}
+
+
+var data = getList("test-sdsd-1", resp.infos);
+
+return data;
+
